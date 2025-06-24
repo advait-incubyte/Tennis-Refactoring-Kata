cba453d1e3c736f2c98227608258431398cd07c8
Advait Maindalkar <advait@incubyte.co>
2025-06-25 01:36:58 +0530

    refactor: moved logic to functions for SRP, and converted magic numbers to named constants

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -1,6 +1,9 @@
 import type { TennisGame } from './types/tennis-game';
 import type { Player } from './types/player';
 
+const DEUCE_THRESHOLD = 3;
+const WIN_THRESHOLD = 3;
+
 export class TennisGame1 implements TennisGame {
   private playerOne: Player;
   private playerTwo: Player;
@@ -18,27 +21,46 @@ export class TennisGame1 implements TennisGame {
   getScore(): string {
     const [playerOneScore, playerTwoScore] = [this.playerOne.score, this.playerTwo.score];
 
-    if (playerOneScore === playerTwoScore) {
-      const score = (playerOneScore < 3) ? (this.getScoreString(playerOneScore) + '-All') : 'Deuce';
-      return score;
-    }
+    if (this.arePlayersPointsTied(playerOneScore, playerTwoScore)) 
+      return this.deuceScore(playerOneScore);
 
-    if (playerOneScore >= 4 || playerTwoScore >= 4) {
-      let score: string = '';
-      const difference: number = Math.abs(playerOneScore - playerTwoScore);
-      const playerWithHigherScore = (playerOneScore > playerTwoScore) ? this.playerOne : this.playerTwo;
-
-      if (difference > 1) {
-        score = `Win for ${playerWithHigherScore.name}`;
-      } else {
-        score = `Advantage ${playerWithHigherScore.name}`;
-      }
-
-      return score;
+    if (this.isAdvantageOrWinPhase(playerOneScore, playerTwoScore)) {
+      return this.advantageOrWinScore(playerOneScore, playerTwoScore);
     } 
 
-    const score = this.getScoreString(playerOneScore) + '-' + this.getScoreString(playerTwoScore);
+    return this.regularScore(playerOneScore, playerTwoScore);
+  }
+  
+  private regularScore(playerOneScore: number, playerTwoScore: number): string {
+    return this.getScoreString(playerOneScore) + '-' + this.getScoreString(playerTwoScore);
+  }
+
+  private isAdvantageOrWinPhase(playerOneScore: number, playerTwoScore: number) {
+    return playerOneScore > WIN_THRESHOLD || playerTwoScore > WIN_THRESHOLD;
+  }
+  
+  private advantageOrWinScore(playerOneScore: number, playerTwoScore: number) {
+    let score: string = '';
+    const difference: number = Math.abs(playerOneScore - playerTwoScore);
+    const playerWithHigherScore = (playerOneScore > playerTwoScore) ? this.playerOne : this.playerTwo;
+
+    if (difference > 1) {
+      score = `Win for ${playerWithHigherScore.name}`;
+    } else {
+      score = `Advantage ${playerWithHigherScore.name}`;
+    }
+
     return score;
+  }
+
+  private arePlayersPointsTied(playerOneScore: number, playerTwoScore: number): boolean {
+    return playerOneScore === playerTwoScore;
+  }
+
+  private deuceScore(playerPoints: number): string {
+    return (playerPoints < DEUCE_THRESHOLD) 
+      ? (this.getScoreString(playerPoints) + '-All') 
+      : 'Deuce';
   }
 
   private getScoreString(score: number): string {
